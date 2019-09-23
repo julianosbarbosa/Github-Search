909623c00998b6908d28ed5bf5b2d88476c711e8
Juliano <julianotrabalho2016@gmail.com>
2019-09-23 20:22:07 -0300

    UPDATE - layout main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Routes from './routes';
 import GlobalStyle from './style/global';
 
 const App = () => (
-  <Fragment>
+  <>
     <Routes />
     <GlobalStyle />
-  </Fragment>
+  </>
 );
 
 export default App;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Title } from './style';
+import { Container, Form, SubmitButton } from './style';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-const Main = () => <Title>Main</Title>;
-
+const Main = () => (
+  <Container>
+    <h1>
+      <FaGithubAlt />
+      Repositorios
+    </h1>
+    <Form>
+      <input type="text" placeholder="Adcione um repositorio" />
+      <SubmitButton disabled>
+        <FaPlus color="#fff" size={14} />
+      </SubmitButton>
+    </Form>
+  </Container>
+);
 export default Main;
