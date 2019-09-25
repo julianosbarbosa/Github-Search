10544273470940495f240705587c67d506df99de
Juliano <julianotrabalho2016@gmail.com>
2019-09-25 11:23:45 -0300

    UPDATE - Layout issues list

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssuesList } from './styles';
 import Container from '../../components/Container/index';
 
 class Repository extends Component {
@@ -44,6 +44,19 @@ class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssuesList>
       </Container>
     );
   }
